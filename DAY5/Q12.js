f328560271a0a93b2cdf6128857fb883ebbfe591
Himani Gohil <himanigohil0002@gmail.com>
2024-10-24 16:55:14 +0530

    Delete DAY5 directory

--- DAY5/Q12.js
+++
@@ -1,7 +0,0 @@
-let a = [22, 3, 3, 2, 1, 2, 3];
-
-tendev = a.every((ei) => {
-  ei % 10 == 0;
-});
-
-console.log(tendev);
--- DAY5/Q5.js
+++
@@ -1,6 +0,0 @@
-let s = 10;
-const arrfun = () => {
-  console.log("Square :- " + s * s);
-};
-
-arrfun();
