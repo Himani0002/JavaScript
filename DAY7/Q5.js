ea9eebd1251950953898aba24b716fbb132c1e51
Himani Gohil <himanigohil0002@gmail.com>
2024-11-07 16:17:32 +0530

    HG | 7/11/24 | DAY7

--- DAY7/Q5.js
+++ DAY7/Q5.js
@@ -75,22 +75,39 @@ h1 = document.querySelector("h1");
 function changeColor(color, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("promise rejected");
+      }
       h1.style.color = color;
       resolve("color was change");
     }, delay);
   });
 }
 
-changeColor("pink", 1000)
-  .then(() => {
-    return changeColor("red", 1000);
-  })
-  .then(() => {
-    return changeColor("purple", 1000);
-  })
-  .then(() => {
-    return changeColor("green", 1000);
-  })
-  .then(() => {
-    return changeColor("yellow", 1000);
-  });
+// changeColor("pink", 1000)
+//   .then(() => {
+//     return changeColor("red", 1000);
+//   })
+//   .then(() => {
+//     return changeColor("purple", 1000);
+//   })
+//   .then(() => {
+//     return changeColor("green", 1000);
+//   })
+//   .then(() => {
+//     return changeColor("yellow", 1000);
+//   });
+
+async function demo() {
+  try {
+    await changeColor("red", 1000);
+    await changeColor("purple", 1000);
+    await changeColor("yellow", 1000);
+    await changeColor("green", 1000);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+demo();
